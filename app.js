e9899910f8ddd16bdf6ffb063619f14da4ebcec6
Macintosh <macintosh@Macintosh.net.fpt>
2018-04-07 21:16:58 +0700

    add port

--- app.js
+++ app.js
@@ -91,5 +91,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+app.listen(3000,function(){
+  console.log('your app is started');
+});
 module.exports = app;
